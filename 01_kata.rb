3ee7c4d85ae6a6760a7ef3ab1aab4b7142eac6d5
Amanda <ajnikrant@gmail.com>
2022-05-05 22:58:54 -0400

    day 1 challenge complete

--- 01_kata.rb
+++ 01_kata.rb
@@ -1,3 +1,25 @@
-def test
-    puts "test"
-end
+#https://www.codewars.com/kata/5813d19765d81c592200001a/train/ruby
+
+def reverse_it(start_, end_)
+
+    arr= (start_ .. end_).to_a
+    n=0
+    f=0
+    
+      while n < arr.length do
+        test = arr[n].to_s.split('')
+        if test.include?('5')
+          f+=1
+        end
+        n = n + 1
+      end
+    
+     
+      
+      puts  arr.size-f
+    
+    end
+    
+    puts reverse_it(1,3)
+    puts reverse_it(1,20)
+    puts reverse_it(4,10)
